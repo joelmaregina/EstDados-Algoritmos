9c8cc7de6caf45740f73b1d5c20c878edff556a4
Joelma Regina <84204532+joelmaregina@users.noreply.github.com>
2022-08-30 23:47:06 -0300

    Updated Questão 6

--- revisao-C.c
+++ revisao-C.c
@@ -117,44 +117,47 @@ int main()
     puts(nomesLS);
 }
 
-// 06. Armazenar nomes e nota 1, 2, de 15 alunos. calcular e armazenar a media. armazenar tambem a situação do aluno: AP ou RP.
-// imprimir uma listagem contendo nome, notas, media e a situação de cada aluno.
-
-#define NUMEROALUNOS 5
-#define TAMNOME 20
-#define TAMNOTAS 2
-int main()
-{
-    char nome[TAMNOME];
-    char nomes[TAMNOME][NUMEROALUNOS];
-    float notas[TAMNOTAS][NUMEROALUNOS];
-    float nota1, nota2;
-    float media;
-    float medias[NUMEROALUNOS];
-    int i, j, k;
-    // ****************** INCOMPLETA!!! ***************************************
-    for (i = 0; i < NUMEROALUNOS; i++){
-        printf("Digite o nome do %d aluno: ", i+1);
-        fgets(nome, TAMNOME, stdin);
-        for(j = 1; nome[j] != '\0'; j++);
-        for(k = 0; k < j; j++){
-            nomes[k][i] = nome[k];
-        }
+// 06. Armazenar nomes e nota 1, 2, de 15 alunos. calcular e armazenar a media. Armazenar tambem a situação do aluno: AP ou RP. 
+// Imprimir uma listagem contendo nome, notas, media e a situação de cada aluno.
+#define NUMEROALUNOS 15
+#define TAMNOME 41
+int main(){
+    struct notasAlunos
+    {
+        float nota1, nota2;
+        float media;
+        char nome[TAMNOME], situacao[4];
+    }alunos[NUMEROALUNOS];
+    
+    for(int i=0; i < NUMEROALUNOS; i++){
+        printf("Digite o nome do %dº aluno: ", i+1);
+        fgets(alunos[i].nome, TAMNOME, stdin);
+            
         printf("Digite a primeira nota do aluno: ");
-        scanf("%f", &nota1);
-        notas[i][0] = nota1;
+        scanf("%f", &alunos[i].nota1);
         
         printf("Digite a segunda nota do aluno: ");
-        scanf("%f", &nota2);
-        notas[i][1] = nota1;
+        scanf("%f", &alunos[i].nota2);
+        getchar();
         
-        media = (nota1 + nota2)/2;
-        medias[i] = media; 
+        printf("------------------------ \n");
+            
+        alunos[i].media = (alunos[i].nota1 + alunos[i].nota2)/2;
+            
+        if(alunos[i].media > 5){
+            strcpy(alunos[i].situacao,"AP.");
+        } else {
+            strcpy(alunos[i].situacao,"RP.");
+        }
     }
     
-    for(i = 0; i < NUMEROALUNOS; i++){
-        printf("-----------------------");
-        printf("Aluna: ");
+    for( int j = 0; j < NUMEROALUNOS; j++) {
+        printf("\n ======= Estudante número %d  ======= ", j+1);
+        printf("\n Nome: %s", alunos[j].nome);
+        printf("\n Nota 1: %.2f", alunos[j].nota1);
+        printf("\n Nota 2: %.2f", alunos[j].nota2);
+        printf("\n Média: %.2f", alunos[j].media);
+        printf("\n Situação: %s", alunos[j].situacao);
     }
 }
 
